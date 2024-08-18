c8ab5e9abaf600bf8ccdb06561ae3830a270e306
deep-hwan <deep@deepcomu.com>
2024-08-18 22:33:32 +0900

    1.0.0

--- dist/widgets/TxtSpan.js
+++ dist/widgets/TxtSpan.js
@@ -49,7 +49,7 @@ function TxtSpan(props) {
     var txt_theme = (0, view_1.TxtTheme)(__assign(__assign(__assign({}, restProps), { size: typeof size === "string" ? size : (size !== null && size !== void 0 ? size : 15) / 16 + "rem", weight: TYPOGRAPH_WEIGHT[weight !== null && weight !== void 0 ? weight : "normal"].fontWeight, color: restProps.color, txtAlign: (_d = restProps.txtAlign) !== null && _d !== void 0 ? _d : "start", whiteSpace: ((_e = props === null || props === void 0 ? void 0 : props.ellipsis) === null || _e === void 0 ? void 0 : _e.ellipsis)
             ? "normal"
             : (_f = props.whiteSpace) !== null && _f !== void 0 ? _f : "pre-line", userSelect: props.userSelect ? props.userSelect : onClick && "none", cursor: props.cursor ? props.cursor : onClick && "pointer" }), (((_g = restProps.ellipsis) === null || _g === void 0 ? void 0 : _g.ellipsis) && txt_ellipsis_extend)));
-    var globel_theme = __assign(__assign(__assign({}, txt_theme), mq_styles), { marginBlockStart: 0, marginBlockEnd: 0, "&:hover": (0, view_1.TxtTheme)(__assign({}, restProps.hover)), "&:active": (0, view_1.TxtTheme)(__assign({}, restProps.active)), "&:disabled": (0, view_1.TxtTheme)(__assign({}, restProps.disabled)) });
+    var globel_theme = __assign(__assign(__assign({}, txt_theme), mq_styles), { "&:hover": (0, view_1.TxtTheme)(__assign({}, restProps.hover)), "&:active": (0, view_1.TxtTheme)(__assign({}, restProps.active)), "&:disabled": (0, view_1.TxtTheme)(__assign({}, restProps.disabled)) });
     //
     //
     return ((0, jsx_runtime_1.jsx)("span", __assign({ className: "span_txt", css: __assign({}, globel_theme) }, elementProps, { children: props.children })));
